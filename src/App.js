34a6fa5e713291446d251f54097487d8cfbae92e
HeatherJLewis <heather@heatherlewis.co.uk>
2019-01-05 19:00:48 +0000

    Header changed to us flexbox structure

--- src/App.js
+++ src/App.js
@@ -7,10 +7,19 @@ class App extends Component {
   render() {
     return (
       <div className="container" style={styles}>
-    
-        <Header />
+      <div className="row">
+                    <div className="col-md-12 col-lg-8">
+            <Header />
+              </div>
+            <div class="col-lg-4 d-none d-lg-block">
+                    <p>Hello There!</p>
+                </div>
+            </div>
+        {/* <Header /> */}
         <TaskInput />
+        <p>Keep It Simple, Stupid</p>
         <TaskList />
+        <Header />
       </div>
     );
   }
@@ -19,3 +28,6 @@ const styles = {
     backgroundColor: "#81E4BD"
 }
 export default App;
+
+            // <img src="images/23823171-illustration-of-a-pink-flower-on-a-white-background.jpg
+            //         " alt="Flower" style="width:360px;height:360px;"> 
