d0aeda998fdee91e909bc580e836037d053b1620
HeatherJLewis <heather@heatherlewis.co.uk>
2019-01-06 17:56:59 +0000

    Event handler added - submitted text sent to an alert

--- src/components/TaskInput.js
+++ src/components/TaskInput.js
@@ -3,27 +3,42 @@ import React from 'react';
 class TaskInput extends React.Component {
     constructor(props) {
         super(props);
+
+        this.state = {
+            taskDescription: ""
+        };
+    
+        //Event binding functions
         this.onSubmitClicked = this.onSubmitClicked.bind(this);
+        this.onTextFieldUpdated = this.onTextFieldUpdated.bind(this);
     }
+    //Function fires when Submit button clicked
     onSubmitClicked() {
-        alert('Hello There');
+        alert(this.state.taskDescription);
     }
+
+    //Function fires when the text box is filled
+    onTextFieldUpdated(event) {
+        const description = event.target.value;
+        this.setState ({
+                taskDescription: description
+        });
+    }
+
     render() {
         return (
-            <form>
                 <div className="form-group">
                   <div className="container">
                       <div className="row" style={styles}>
                           <div className="col-sm-12 col-md-11">
-                             <input type="taskAdd" className="form-control" id="taskToAdd" aria-describedby="taskAdd" placeholder="Add your next task here. Keep it SMART!" />
+                             <input type="text" value={this.state.taskDescription} className="form-control" placeholder="Add your next task here. Keep it SMART!" onChange={this.onTextFieldUpdated} />
                           </div>
                           <div className="col-sm-1 col-md-1">
                             <button type="submit" className="btn btn-primary" onClick={this.onSubmitClicked} >Submit</button>
                           </div>
                       </div>
                       </div>
-                      </div>
-                  </form>
+                </div>
         );
     }
 
@@ -33,3 +48,5 @@ const styles = {
     paddingBottom : "10px",
 }
 export default TaskInput;
+
+//  id="taskToAdd" aria-describedby="taskAdd"
