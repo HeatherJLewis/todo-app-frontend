2fb13dd8b9d2a065b1a76507a71e6f66836ee23e
HeatherJLewis <heather@heatherlewis.co.uk>
2019-01-12 09:23:00 +0000

    Separate TaskDeleteButton Component Created

---
+++ src/components/TaskDeleteButton.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+class TaskDeleteButton extends React.Component {
+
+    constructor(props) {
+        super(props);
+    this.onDeleteClicked = this.onDeleteClicked.bind(this);
+    }
+
+    onDeleteClicked () {
+        alert('Are you sure?');
+    }
+
+    render() {
+        return (
+            <div>
+                <input className="btn btn-primary" type="submit" value="Delete" onClick={this.onDeleteClicked} />
+            </div>
+        );
+    }
+
+};
+const styles = {
+    paddingTop : "10px",
+    paddingBottom : "10px"
+}
+export default TaskDeleteButton;
