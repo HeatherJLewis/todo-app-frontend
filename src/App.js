db4fa080e89350b1e45af2807540d4b26047ea52
HeatherJLewis <heather@heatherlewis.co.uk>
2019-02-24 18:22:09 +0000

    Task count now works and changes on all event handlers

--- src/App.js
+++ src/App.js
@@ -18,35 +18,37 @@ class App extends Component {
     this.addTask = this.addTask.bind(this);
     this.taskDone = this.taskDone.bind(this);
     this.taskDelete = this.taskDelete.bind(this);
-
   }
 
   async componentDidMount() {
     const tasks = await TasksService.getTasks();
+    const tasksStillLeft = await TasksService.getTasksLeft();
+    const taskCount = tasksStillLeft[0]['COUNT(*)'];
+    this.setState({ tasksLeft: taskCount });
     // console.log(tasks);
     this.setState({ tasks: tasks });
-
-    const tasksStillLeft = await TasksService.getTasksLeft();
-    console.log(tasksStillLeft);
-    this.setState({tasksLeft: tasksStillLeft})
   }
 
   async addTask(task) {
-
+    const tasksStillLeft = await TasksService.getTasksLeft();
+    const taskCount = tasksStillLeft[0]['COUNT(*)'];
+    this.setState({ tasksLeft: taskCount });
     const response = await TasksService.saveTask(task);
     // console.log(response);
     task.taskId = response.insertId;
 
     this.setState({
       tasks: [...this.state.tasks, task]
-    })
+          })
   }
   //I want to iterate through to find the id that matches with the task I want to delete and set the completed status to true
   //Id needs to come from tasks.id and needs to be drawn from the taskList component
 
   async taskDone(taskId) {
     const response = await TasksService.updateTask(taskId);
-
+    const tasksStillLeft = await TasksService.getTasksLeft();
+    const taskCount = tasksStillLeft[0]['COUNT(*)'];
+    this.setState({ tasksLeft: taskCount });
     this.setState({
       tasks: this.state.tasks.map(tasks => {
         if (tasks.taskId === taskId) {
@@ -63,11 +65,13 @@ class App extends Component {
 
   async taskDelete(taskId) {
     const response = await TasksService.deleteTask(taskId);
-
+    const tasksStillLeft = await TasksService.getTasksLeft();
+    const taskCount = tasksStillLeft[0]['COUNT(*)'];
+    this.setState({ tasksLeft: taskCount });
     this.setState({
       tasks: this.state.tasks.filter(tasks => tasks.taskId !== taskId)
     });
-};
+  };
 
   // async tasksLeftToDo() {
   //   const tasksStillLeft = await TasksService.getTasksLeft();
