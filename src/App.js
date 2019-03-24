f2aa6489758c3a62c2b137872ac89e0418075b5c
HeatherJLewis <heather@heatherlewis.co.uk>
2019-03-24 17:40:14 +0000

    Bug fix: Tasks Left updates on adding a task

--- src/App.js
+++ src/App.js
@@ -30,15 +30,13 @@ class App extends Component {
   }
 
   async addTask(task) {
+    const response = await TasksService.saveTask(task);
+    task.taskId = response.insertId;
     const tasksStillLeft = await TasksService.getTasksLeft();
     const taskCount = tasksStillLeft[0]['COUNT(*)'];
-    this.setState({ tasksLeft: taskCount });
-    const response = await TasksService.saveTask(task);
-    // console.log(response);
-    task.taskId = response.insertId;
-
     this.setState({
-      tasks: [...this.state.tasks, task]
+      tasks: [...this.state.tasks, task],
+      tasksLeft: taskCount
           })
   }
   //I want to iterate through to find the id that matches with the task I want to delete and set the completed status to true
@@ -72,12 +70,6 @@ class App extends Component {
       tasks: this.state.tasks.filter(tasks => tasks.taskId !== taskId)
     });
   };
-
-  // async tasksLeftToDo() {
-  //   const tasksStillLeft = await TasksService.getTasksLeft();
-  //   console.log(tasksStillLeft);
-  //   this.setState({tasksLeft: tasksStillLeft})
-  // };
 
   render() {
     return (
